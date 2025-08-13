f9df191ed905dcc30e03ab7efa1db9519bf3ffd8
47anjan <anjankarmakar15@gmail.com>
2025-08-14 00:16:34 +0600

    disabled forceConsistentCasingInFileNames

--- app/dashboard/connections/page.tsx
+++ app/dashboard/connections/page.tsx
@@ -4,9 +4,9 @@ import React, { useState } from "react";
 import { Users, UserPlus, Clock } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-import Pending from "@/components/connections/pending";
-import Requests from "@/components/connections/requests";
-import Friends from "@/components/connections/friends";
+import Requests from "@/components/connections/Requests";
+import Pending from "@/components/connections/Pending";
+import Friends from "@/components/connections/Friends";
 
 const ConnectionsPage = () => {
   const [activeTab, setActiveTab] = useState("friends");
